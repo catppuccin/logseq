ed060c3cd868683709d8bd94776f4651d709d757
Soumik Pradhan <10350864+griimick@users.noreply.github.com>
2022-11-05 19:23:09 +0530

    feat: add dev setting to autoreload css

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var timer = null;
+
+function reloadCss() {
+    var links = parent.document.getElementsByTagName("link");
+    var link = Array.from(links).find(l => l.href.includes("ctp"));
+    link.href += "";
+}
+
 function main() {
     const settings = [
         {
@@ -9,13 +17,27 @@ function main() {
             enumChoices: ["Full palette", "Rosewater", "Flamingo", "Pink", "Mauve", "Red", "Maroon", "Peach", "Yellow", "Green", "Teal", "Sky", "Sapphire", "Blue", "Lavendar"],
             default: "Full Palette"
         },
+        {
+            key: "CtpReloadCss",
+            title: "Developer Mode: Reload CSS",
+            type: "boolean",
+            default: false
+        }
     ]
     logseq.useSettingsSchema(settings);
     logseq.onSettingsChanged(updatedSettings => {
         logseq.App.showMsg(`Applied ${updatedSettings.CtpAccent} accent✨`)
+
+        if (updatedSettings.CtpReloadCss && !timer) {
+            timer = setInterval(reloadCss, 1000)
+        } else if (!updatedSettings.CtpReloadCss && timer) {
+            clearInterval(timer);
+            timer = null;
+        }
         // TODO: Add logic to change accent colors
     });
 }
+
 
 // bootstrap
 logseq.ready(main).catch(console.error)
