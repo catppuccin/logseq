5643bccec59a22dd4ba192a271a7b9995774f668
Soumik <10350864+griimick@users.noreply.github.com>
2024-03-10 02:33:32 +0530

    feat: setting to override whiteboard to latte (#30)
    * organise whiteboard vars
    
    * remove hardcoded var in main
    
    * feat: add whiteboard override setting
    
    * whiteboard latte override working v1
    
    * latte override working 2
    
    * more minor changes
    
    * fix: minor
    
    * fix: whiteboard fill colors
    
    * fix: whiteboard fill colors

--- index.js
+++ index.js
@@ -29,13 +29,41 @@ const settings = [
         enumChoices: accentNames,
         default: "Full palette"
     },
+    {
+        key: "CtpWhiteboard",
+        title: "Override Whiteboard theme to light theme?",
+        description: "  Override whiteboard theme to use Latte theme flavor",
+        type: "boolean",
+        default: false,
+    },
 ];
+
+function setWhiteboardOverride(bool) {
+    const rootContainer = parent.document.querySelector(`html`);
+    if (bool) {
+        rootContainer.classList.add('whiteboard-latte');
+    } else {
+        rootContainer.classList.remove('whiteboard-latte');
+    }
+}
 
 function setAccent(accentName) {
     logseq.provideStyle({
         key: 'ctp-accent',
         style: `
           :root:not([data-color]), :root[data-color='none'], :root[data-color='logseq'] {
+            --ctp-accent: ${accentMap[accentName]};
+          }
+          html.whiteboard-latte div.whiteboard-page {
+            --ctp-accent: ${accentMap[accentName]};
+          }
+          html.whiteboard-latte div.dashboard-card {
+            --ctp-accent: ${accentMap[accentName]};
+          }
+          html.whiteboard-latte div.tl-tooltip-content {
+            --ctp-accent: ${accentMap[accentName]};
+          }
+          html.whiteboard-latte div.tl-select-input-content {
             --ctp-accent: ${accentMap[accentName]};
           }
         `,
@@ -53,6 +81,9 @@ async function main() {
     logseq.onSettingsChanged(updatedSettings => {
         if (setAccent(updatedSettings.CtpAccent)) {
             console.log(`Applied ${updatedSettings.CtpAccent} accent✨`);
+        }
+        if (setWhiteboardOverride(updatedSettings.CtpWhiteboard)) {
+            console.log(`${updatedSettings.CtpWhiteboard ? 'Applied' : 'Removed'} Latte whiteboard flavor✨`);
         }
     });
 }
